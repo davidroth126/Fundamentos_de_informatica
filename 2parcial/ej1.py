298f58fbc3b9c63b04f0c07d92da4fc3ab68cca7
davidroth126 <102978783+davidroth126@users.noreply.github.com>
2022-07-04 10:17:36 -0300

    5

--- 2parcial/ej1.py
+++ 2parcial/ej1.py
@@ -14,4 +14,9 @@ print("Se almacenan:",len(breeds.json()["message"]),"breeds.")
 
 print(breeds.json()["message"][11])
 
-#Esta es la url que esperaria para ver las imagenes de boxer."https://dog.ceo/api/breeds/list/11/images"
+# Esta es la url que esperaria para ver las imagenes de boxer por el hecho de que es el elemnto 11 https://dog.ceo/api/breeds/list/11/images ,
+# tambien podria esperar."https://dog.ceo/api/breeds/list/boxer/images .
+
+https://dog.ceo/api/breeds/list/boxer/images
+
+ https://images.dog.ceo/breeds/boxer/boxer.jpg
