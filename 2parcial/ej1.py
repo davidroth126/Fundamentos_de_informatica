909696ee1909ca1b7f6a2373dcad1395fcf5dee3
davidroth126 <102978783+davidroth126@users.noreply.github.com>
2022-07-04 09:44:34 -0300

    psuh2

--- 2parcial/ej1.py
+++ 2parcial/ej1.py
@@ -5,3 +5,5 @@ r = requests.get("https://dog.ceo/api/breeds/list/all")
 # A_ El dominio al que estamos consultando es dog.ceo .
 
 print("El status code es:",r.status_code) 
+
+print("El content type es:", r.headers["Content-Type"])
